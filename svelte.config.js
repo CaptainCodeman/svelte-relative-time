3d98dc05368399bba988c2cd6bb98177278f66c4
Simon Green <simon@captaincodeman.com>
2023-06-22 10:07:55 -0600

    reusable deploy workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
-const dev = process.argv.includes('dev')
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -11,9 +9,6 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
-		},
 	},
 }
 
